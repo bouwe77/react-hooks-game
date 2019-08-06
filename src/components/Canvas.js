7c2f8ee5af1bb51f32ff02c35d9d530024a2efbe
Bouwe K. Westerdijk <bouwe@bouwe.nl>
2019-08-06 21:07:10 +0200

    .

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -4,10 +4,12 @@ import Background from "../svg/Background";
 import Player from "./Player";
 import Enemy from "./Enemy";
 
-export default ({ settings }) => (
+export default ({ settings, player, enemies }) => (
   <svg width={settings.canvasWidth} height={settings.canvasHeight}>
     <Background settings={settings} />
     <Player settings={settings} initialX={10} initialY={10} />
-    <Enemy settings={settings} initialX={120} initialY={30} />
+    {enemies.map(enemy => (
+      <Enemy settings={settings} initialX={120} initialY={30} />
+    ))}
   </svg>
 );
--- src/components/Game.js
+++ src/components/Game.js
@@ -3,4 +3,10 @@ import React from "react";
 import Canvas from "./Canvas";
 import settings from "../settings";
 
-export default () => <Canvas settings={settings} />;
+function Game() {
+  const player = {};
+  const enemies = [];
+  return <Canvas settings={settings} player={player} enemies={enemies} />;
+}
+
+export default Game;
