68b249c622429da1d2bccae7ff00b728182335e9
Bouwe K. Westerdijk <bouwe@bouwe.nl>
2019-08-08 09:53:24 +0200

    .

--- src/hooks/useEnemies.js
+++ src/hooks/useEnemies.js
@@ -1,11 +1,19 @@
 import { useState } from "react";
+import useAnimation from "./useAnimation";
 
 export function useEnemies() {
+  useAnimation(moveEnemies);
+
   const [enemies, setEnemies] = useState([
     { x: 120, y: 30 },
     { x: 10, y: 180 },
     { x: 310, y: 310 }
   ]);
+
+  function moveEnemies() {
+    const movedEnemies = enemies.map(enemy => ({ ...enemy, x: enemy.x + 1 }));
+    setEnemies(movedEnemies);
+  }
 
   return enemies;
 }
